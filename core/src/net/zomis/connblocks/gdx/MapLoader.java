e6c50204cd2c14fb92eba2d0e84ecfe44c4c5579
Simon Forsberg <zomis2k@hotmail.com>
2014-11-17 00:52:48 +0100

    added support for reading the tiles for a RectangularMapObject. Successfully the positions for a rectangular connection

--- core/src/net/zomis/connblocks/gdx/MapLoader.java
+++ core/src/net/zomis/connblocks/gdx/MapLoader.java
@@ -1,20 +1,31 @@
 package net.zomis.connblocks.gdx;
 
 import com.badlogic.gdx.maps.MapLayer;
+import com.badlogic.gdx.maps.MapObject;
 import com.badlogic.gdx.maps.MapProperties;
+import com.badlogic.gdx.maps.objects.PolygonMapObject;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.math.Rectangle;
 import net.zomis.ConnBlocks;
+import net.zomis.connblocks.Block;
 import net.zomis.connblocks.BlockMap;
+import net.zomis.connblocks.BlockTile;
 import net.zomis.connblocks.BlockType;
 
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Set;
 
 /**
  * Created by Zomis on 2014-11-16.
  */
 public class MapLoader {
+
+    private static final int TILE_SIZE = 32;
 
     public MapLoader() {
     }
@@ -49,10 +60,56 @@ public class MapLoader {
             loadMapLayer(tiled, result, (TiledMapTileLayer) layer);
         }
         else if (layer.getName().toLowerCase().startsWith("conn")) {
-
+            loadConnections(tiled, result, layer);
         }
         else if (layer.getName().toLowerCase().startsWith("spec")) {
+            loadSpecials(tiled, result, layer);
+        }
+    }
 
+    private void loadSpecials(TiledMap tiled, BlockMap result, MapLayer layer) {
+
+    }
+
+    private void loadConnections(TiledMap tiled, BlockMap result, MapLayer layer) {
+        Iterator<MapObject> it = layer.getObjects().iterator();
+        while (it.hasNext()) {
+            MapObject obj = it.next();
+            Set<BlockTile> blocks = tilesForObject(result, obj);
+            result.addConnection(blocks.toArray(new BlockTile[blocks.size()]));
+        }
+    }
+
+    private Set<BlockTile> tilesForObject(BlockMap map, MapObject obj) {
+        Set<BlockTile> result = new HashSet<BlockTile>();
+        if (obj instanceof PolygonMapObject) {
+            PolygonMapObject poly = (PolygonMapObject) obj;
+            poly.getPolygon().getVertices();
+//            throw new UnsupportedOperationException("Polygons will be supported later");
+        }
+        if (obj instanceof RectangleMapObject) {
+            RectangleMapObject rect = (RectangleMapObject) obj;
+            Rectangle bounds = rect.getRectangle();
+            checkMod(obj.getName(), bounds.x, TILE_SIZE);
+            checkMod(obj.getName(), bounds.y, TILE_SIZE);
+            checkMod(obj.getName(), bounds.width, TILE_SIZE);
+            checkMod(obj.getName(), bounds.height, TILE_SIZE);
+            for (int xx = (int) bounds.x; xx < bounds.x + bounds.width; xx += TILE_SIZE) {
+                int x = xx / TILE_SIZE;
+                for (int yy = (int) bounds.y; yy < bounds.y + bounds.height; yy += TILE_SIZE) {
+                    int y = map.getMapHeight() - 1 - yy / TILE_SIZE;
+                    result.add(map.pos(x, y));
+                }
+            }
+
+            return result;
+        }
+        throw new RuntimeException("Unsupported MapObject class: " + obj + " only Polygon and Rectangle are supported");
+    }
+
+    private void checkMod(String name, float value, int mod) {
+        if (value % mod != 0) {
+            throw new RuntimeException("Invalid value for '" + name + "': Expected divisibilty by " + mod + " but was " + value);
         }
     }
 
@@ -65,10 +122,9 @@ public class MapLoader {
             MapProperties prop = (MapProperties) properties;
             String actual = String.valueOf(prop.get(key));
             check(key, actual, expected);
+            return;
         }
-
-
-
+        throw new UnsupportedOperationException("Unsupported properties: " + properties);
     }
 
     private void check(String key, String actual, String expected) {
