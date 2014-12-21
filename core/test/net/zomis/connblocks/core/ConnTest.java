c862ee02499ea3ad0badba1d13b1315d48791edd
Simon Forsberg <zomis2k@hotmail.com>
2014-12-21 23:54:23 +0100

    added simple test

---
+++ core/test/net/zomis/connblocks/core/ConnTest.java
@@ -0,0 +1,22 @@
+package net.zomis.connblocks.core;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Zomis on 2014-12-21.
+ */
+public class ConnTest {
+
+
+    public ConnTest() {
+
+    }
+
+    @Test
+    public void test() {
+        assertFalse(false);
+    }
+
+}
