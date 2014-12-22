fa35fde370d7bb3fcf45d3a725501bb30a2705a2
Simon Forsberg <zomis2k@hotmail.com>
2014-12-22 02:07:18 +0100

    code cleanup

--- core/src/net/zomis/connblocks/gdx/ConnectionActor.java
+++ core/src/net/zomis/connblocks/gdx/ConnectionActor.java
@@ -26,11 +26,7 @@ public class ConnectionActor extends Actor {
     }
 
     private static final Color[] connectColors = new Color[] { Color.BLUE, Color.RED, Color.GREEN, Color.YELLOW,
-            Color.CYAN, Color.ORANGE, Color.TEAL, Color.PURPLE
-//            0xffFF7F00, 0xffBFFF00, 0xff0095FF, 0xffFF00AA, 0xffFFD400, 0xffEDB9B9, 0xffEDB9B9, 0xffE7E9B9,
-            //          0xffDCB9ED, 0xffB9EDE0, 0xff8F2323, 0xff23628F, 0xff8F6A23, 0xff6B238F, 0xff4F8F23, 0xff737373, 0xffCCCCCC,
-            //        0xffff8000, 0xff7700dd, 0xff00ffff, 0xffffaec4
-    };
+            Color.CYAN, Color.ORANGE, Color.TEAL, Color.PURPLE };
 
     private static Color[] getConnColors(ConnectingBlocks conn) {
         if (conn.getConnectGroups() == 16777215)
@@ -69,13 +65,11 @@ public class ConnectionActor extends Actor {
             batch.draw(texture, x * size, y * size, size, size);
         }
         gradientShader.setUniformMatrix("u_projTrans", game.camera.combined);
-//        findTopLeft(connection, po);
         Color[] colors = getConnColors(connection);
         if (colors.length == 1) {
             colors[0].a = color.a;
             gradientShader.setUniformf("colorA", colors[0]);
             gradientShader.setUniformf("colorB", colors[0]);
-//            ConnBlocks.log(connection + " is one colored: " + colors[0]);
             batch.setColor(colors[0]);
             batch.setShader(gradientShader);
         }
@@ -85,7 +79,6 @@ public class ConnectionActor extends Actor {
             Color theColor = getConnColors(connection)[0];
             gradientShader.setUniformf("colorA", colors[0]);
             gradientShader.setUniformf("colorB", colors[1]);
-//            ConnBlocks.log(connection + " is multi colored " + Arrays.toString(colors));
             batch.setColor(theColor);
             batch.setShader(gradientShader);
         }
