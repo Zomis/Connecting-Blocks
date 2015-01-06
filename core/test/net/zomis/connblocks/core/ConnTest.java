78181c6e0b6c0d2d188faa8536037aa02c57537f
Simon Forsberg <zomis2k@hotmail.com>
2015-01-06 20:53:51 +0100

    extracted loadmap method

--- core/test/net/zomis/connblocks/core/ConnTest.java
+++ core/test/net/zomis/connblocks/core/ConnTest.java
@@ -9,10 +9,7 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.badlogic.gdx.files.FileHandle;
 import net.zomis.connblocks.BlockMap;
 import net.zomis.connblocks.gdx.mapload.MapLoader;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.*;
 import org.junit.runner.RunWith;
 
 import java.io.File;
@@ -24,14 +21,24 @@ import static org.junit.Assert.*;
 @RunWith(LibGdxTestRunner.class)
 public class ConnTest {
 
+    public BlockMap loadMap(String name) {
+        try {
+            MapLoader loader = new MapLoader();
+            URL uri = getClass().getClassLoader().getResource(name);
+            URL tilesPng = getClass().getClassLoader().getResource("tiles.png");
+            FileHandle internal = new FileHandle(new File(uri.toURI()));
+            BlockMap map = loader.load(internal, new FileHandle(new File(tilesPng.toURI())));
+            assertNotNull("Unable to load map", map);
+            return map;
+        } catch (URISyntaxException e) {
+            throw new AssertionError(e);
+        }
+    }
+
     @Test
-    public void test() throws URISyntaxException {
-        MapLoader loader = new MapLoader();
-        URL uri = getClass().getClassLoader().getResource("test.tmx");
-        URL tilesPng = getClass().getClassLoader().getResource("tiles.png");
-        FileHandle internal = null;
-        internal = new FileHandle(new File(uri.toURI()));
-        BlockMap map = loader.load(internal, new FileHandle(new File(tilesPng.toURI())));
+    public void test() {
+        System.out.println("test result");
+        BlockMap map = loadMap("test.tmx");
         assertNotNull(map);
         assertEquals(2, map.getConnections().size());
     }
