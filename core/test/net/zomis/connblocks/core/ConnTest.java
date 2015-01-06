665128826ae236a79ad1fb145863a6d4e6a790f9
Simon Forsberg <zomis2k@hotmail.com>
2015-01-06 21:37:05 +0100

    added (skipped and failing) Stack Overflow test, for issue #44

--- core/test/net/zomis/connblocks/core/ConnTest.java
+++ core/test/net/zomis/connblocks/core/ConnTest.java
@@ -7,7 +7,10 @@ import com.badlogic.gdx.backends.headless.HeadlessApplicationConfiguration;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.badlogic.gdx.files.FileHandle;
+import net.zomis.connblocks.Block;
 import net.zomis.connblocks.BlockMap;
+import net.zomis.connblocks.ConnectingBlocks;
+import net.zomis.connblocks.Direction4;
 import net.zomis.connblocks.gdx.mapload.MapLoader;
 import org.junit.*;
 import org.junit.runner.RunWith;
@@ -20,6 +23,16 @@ import static org.junit.Assert.*;
 
 @RunWith(LibGdxTestRunner.class)
 public class ConnTest {
+
+    @Test
+    @Ignore
+    public void stackOverflow() {
+        // this currently causes Stack Overflow error because connection is just going back-and-forth
+        BlockMap map = loadMap("stackoverflow-moveup.tmx");
+        assertEquals(1, map.getConnections().size());
+        boolean moveResult = map.getConnections().iterator().next().move(Direction4.UP);
+        assertTrue(moveResult);
+    }
 
     public BlockMap loadMap(String name) {
         try {
